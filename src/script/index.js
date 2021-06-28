385067723188874cc14e2fd10565b43b481a6b20
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-06-28 20:52:11 -0300

    api connection

--- src/script/index.js
+++ src/script/index.js
@@ -1,24 +1,12 @@
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn-click');
-btn.addEventListener('click', (e) => {
-
-    const value = input.value.replace(' ', '+');
-    const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}`
-
-    fetch(URL)
-    .then(res => res.json())
-    .then((out) => {
-      const {items} = out;
-      console.log(items);
-    })
-    .catch(err => { throw err });
-
-    console.log(URL);
-
-});
-
 
 class Response {
+
+    constructor() {
+
+        this.book = []
+    }
 
     start() {
 
@@ -28,8 +16,43 @@ class Response {
 
     async Search() {
 
-    
-       
+        btn.addEventListener('click', (e) => {
+
+            const value = input.value.replace(' ', '+');
+            const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}`
+        
+            fetch(URL)
+            .then(res => res.json())
+            .then((out) => this.loadElements(out)).catch(err => { throw err });
+        
+            console.log(URL);
+        
+        });
+    }
+
+    async loadElements(json) {
+
+        const {items} = json;
+        const table = document.createElement('table');
+
+        for(let i in items) {
+
+            
+            const {volumeInfo} = items[i];
+            const {imageLinks} = volumeInfo;
+            //const {smallThumbnail} = imageLinks;
+            const {authors} = volumeInfo;
+            const {title} = volumeInfo;
+
+            const tr = document.createElement('tr');
+            let td = document.createElement('td');
+            td.innerHTML = title;
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+
+        const result = document.querySelector('.result');
+        result.appendChild(table);
     }
 }
 
