ef5287ad7e865b273274f1b768ee8a4bf82aec8c
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-06-28 21:42:30 -0300

    Api config

--- src/script/index.js
+++ src/script/index.js
@@ -1,22 +1,6 @@
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn-click');
-btn.addEventListener('click', (e) => {
-
-    const value = input.value.replace(' ', '+');
-    const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}`
-
-    fetch(URL)
-    .then(res => res.json())
-    .then((out) => {
-      const {items} = out;
-      console.log(items);
-    })
-    .catch(err => { throw err });
-
-    console.log(URL);
-
-});
-
+const section = document.querySelector('#UnderBox');
 
 class Response {
 
@@ -28,8 +12,65 @@ class Response {
 
     async Search() {
 
-    
-       
+
+        btn.addEventListener('click', (e) => {
+
+            section.classList.add('Result-box');
+            
+            const value = input.value.replace(' ', '+').toLowerCase();
+            const URL = `https://www.googleapis.com/books/v1/volumes?q=${value}`
+        
+            fetch(URL)
+            .then(res => res.json())
+            .then((out) => this.loadElements(out)).catch(err => { throw err });
+        
+            console.log(URL);
+        
+        });
+    }
+
+    async loadElements(json) {
+
+        const {items} = json;
+        const table = document.createElement('table');
+
+        for(let i in items) {
+
+            try{
+                const {volumeInfo} = items[i];
+                const {imageLinks} = volumeInfo;
+                console.log(imageLinks);
+                const ThumbNails = imageLinks.thumbnail; 
+                const {authors} = volumeInfo;
+                const {title} = volumeInfo;
+
+                const tr = document.createElement('tr');
+
+                let TittleTd = document.createElement('td');
+                TittleTd.innerHTML = title;
+                tr.appendChild(TittleTd);
+
+                let ImageTd = document.createElement('td');
+                const image = document.createElement('img');
+                image.classList.add('Image');
+                image.src = ThumbNails
+                ImageTd.appendChild(image);
+                tr.appendChild(ImageTd);
+                table.appendChild(tr)
+
+            } catch(e) {
+
+                console.log(e);
+
+            } finally{}
+        }
+
+        const result = document.querySelector('.result');
+        if(result.hasChildNodes()){
+            result.removeChild(result.lastChild);
+        } 
+        result.appendChild(table);
+
     }
 }
 
